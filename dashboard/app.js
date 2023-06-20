3a9ea5fb684387de55162b63ee5d9c963583b732
Amirkhan567 <amrkh5678@gmail.com>
2023-06-20 13:10:20 +0500

    user detail added

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,13 +1,72 @@
 let logout = document.getElementById('logout');
+let inputFileUpload = document.getElementById('inputFileUpload');
+let photoIcon = document.getElementById('photoIcon');
+let postBtn = document.getElementById('postBtn');
+let textPost = document.getElementById('textPost');
+let ContentBox = document.getElementById('ContentBox');
+// console.log(textPost)
+
+// userdetail
+let userName = document.querySelector('.userName');
+console.log(userName.innerHTML)
+let postText = document.querySelector('.postText');
+let userImg = document.querySelector('.userImg');
 
 
+// date
+
+const time = new Date().getHours()
+// console.log(time)
 
 const activeUser = JSON.parse(localStorage.getItem('activeUser')) || [];
 // console.log(activeUser);
+const {email,firstName,lastName,password,phoneNum} = activeUser
 
+// console.log(email)
+console.log(firstName)
+// console.log(lastName)
+// console.log(password)
+// console.log(phoneNum)
+
+if(!activeUser){
+    window.location.href = "/index.html";
+}
+
+// fileupload
+
+photoIcon.addEventListener('click', fileOpenHandler)
+function fileOpenHandler(){
+    inputFileUpload.click()
+}
+
+// logout function
 
 logout.addEventListener('click',logoutHandler)
 
 function logoutHandler(){
     location.href = "/index.html";
 }
+
+// for post function
+userName.innerHTML = `${firstName} ${lastName}`;
+
+postBtn.addEventListener('click', ()=> {
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'postContentDiv mt-5 mb-3' )
+    
+    div.innerHTML = `
+    <div class="postArea container-fluid py-2 rounded-2 d-flex direction-column">
+    <img class="userImg" src="../Assets/profile pic.jfif" alt="">
+    <p class="userName mt-2">${activeUser.firstName} ${activeUser.lastName}</p>
+    <p id="postTime">${time}h</p>
+    <p class="postText mt-2">${textPost.value}</p>
+  </div>
+  <div class="postImage mt-4">
+    <img class="img-fluid" src="../Assets/photo-1481349518771-20055b2a7b24.jfif" alt="">
+  </div>
+    `
+    ContentBox.prepend(div)
+    textPost.value = ''
+
+})
